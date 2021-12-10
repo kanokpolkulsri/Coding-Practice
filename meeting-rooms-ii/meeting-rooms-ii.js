a236cc08fc7670bc601bdb9a1ada2d25f4610659
Palm Kanokpol <kanokpolkulsri@gmail.com>
2021-12-10 17:48:09 +0700

    Time: 84 ms (76.52%), Space: 40.7 MB (82.45%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.js
+++ meeting-rooms-ii/meeting-rooms-ii.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    /*let start = intervals.sort((a,b) => {return a[0]-b[0]});
+    let start = intervals.slice().sort((a,b) => {return a[0]-b[0]});
     let end = intervals.sort((a,b) => {return a[1]-b[1]});
     let room=0, endi=0;
     
@@ -11,16 +11,5 @@ var minMeetingRooms = function(intervals) {
         start[i][0] < end[endi][1] ? room++ : endi++;
     }
     return room;
-    */
-    var starts = intervals.slice().sort((a, b) => a[0] - b[0]);
-    var ends = intervals.sort((a, b) => a[1] - b[1]);
-
-    var rooms = 0;
-    var end = 0;
-
-    for (var start = 0; start < intervals.length; start++) {
-		starts[start][0] < ends[end][1] ? rooms++ : end++;
-    }
-
-    return rooms;
+    
 };
