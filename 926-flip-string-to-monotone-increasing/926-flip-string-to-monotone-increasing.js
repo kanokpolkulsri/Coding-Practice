e010efb0651d5974f7ed15d53c857a97ef5d30d2
Palm Kanokpol <kanokpolkulsri@gmail.com>
2022-01-04 17:18:46 +0700

    Time: 96 ms (47.46%), Space: 42.2 MB (88.70%) - LeetHub

---
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minFlipsMonoIncr = function(s) {
+    let count = 0;
+    for(let i of s) {
+        if(i === '0') count += 1;
+    }
+    let tmp = count;
+    for(let i of s) {
+        if(i === '0') tmp -= 1;
+        else tmp += 1;
+        count = Math.min(count, tmp);
+    }
+    return count;
+};
